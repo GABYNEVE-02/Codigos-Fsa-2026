d841e6c9ee9d4b57ee91a28788e5f9027c0f2f9f
Gabriel Andrade de Oliveira <48766415870@graduacao.fsa.br>
2026-08-22 14:12:03 -0300

    Troca entre objs

---
+++ Estrutura de Dados/Aula02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template<class T>
+class Item {
+    public:
+        T storage[50];
+};
+
+
+
+int main() {
+
+    Item<int> intObj;
+    Item<float> floatObj;
+    
+    std::cin >> intObj.storage[1];
+    std::cin >> floatObj.storage[1];
+
+    std::cout << intObj.storage[1] << std::endl;
+    std::cout << floatObj.storage[1] << std::endl;
+
+
+
+
+
+
+
+    return 0;
+}
